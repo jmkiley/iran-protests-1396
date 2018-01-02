8d1ba50af5bf09d24aa2d06a2b6aa8af814ed86e
jmkiley <jordan.kiley@mapbox.com>
2018-01-02 09:35:10 -0800

    First commit

---
+++ IranProtests2017Tests/IranProtests2017Tests.swift
@@ -0,0 +1,36 @@
+//
+//  IranProtests2017Tests.swift
+//  IranProtests2017Tests
+//
+//  Created by Jordan Kiley on 1/2/18.
+//  Copyright © 2018 Jordan Kiley. All rights reserved.
+//
+
+import XCTest
+@testable import IranProtests2017
+
+class IranProtests2017Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
